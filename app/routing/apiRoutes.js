1632e07ae3c8119590395c85fdab3dd509d4d8ec
Molly Zurheide <you@example.com>
2018-06-17 18:55:20 -0400

    working on modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,36 +3,27 @@ var friends = require("../data/friends.js"); //pulling in friends data
 
 module.exports = function(app) {
 
-  app.get("/api/friends", function(req, res) {  //when get request received, return JSON of friends already entered
-      //console.log("hello") //ok
+  app.get("/api/friends", function(req, res) {  //return JSON of friends already entered
       res.json(friends);
     });
 
 	app.post("/api/friends", function(req, res) { //once post request is received by server...
-    //console.log("hi"); //ok
     
-    var bestMatch = {
+    var bestMatch = { //current best match
       name: "",
       photo: "",
       diff: 100,  
     }
  
-    var newUserScores = req.body.scores;
-    console.log("new user's scores: " + newUserScores)
+    var newUserScores = req.body.scores; //array of scores for user most recently entered
 
     for (var i=0; i<friends.length - 1; i++) { //compare array of scores to those of each other friend saved
-      var scoreArray = friends[i].scores; //other friends' score arrays
-      
-      var totalDifference = 0;
-
+      var scoreArray = friends[i].scores; //other friends' score arrays      
+      var totalDifference = 0; //initial value
       console.log("scoreArray: " + scoreArray);
 
-      //var totalDifferenceArray = [];
-      var diffArray = [];
-
       for (var j=0; j<scoreArray.length; j++) { //loop through each question to calculate difference in score
-
-        totalDifference += Math.abs(parseInt(newUserScores[j]) - parseInt(scoreArray[j])); //adding difference between each question's score to calculate total score differential
+        totalDifference += Math.abs(parseInt(newUserScores[j]) - parseInt(scoreArray[j])); //calculating total score differential for that friend
 
         if (totalDifference <= bestMatch.diff) { //if diff is lower than that of current best match...
           bestMatch.name = friends[i].name; //that friend is best match, so assign name,
@@ -44,9 +35,8 @@ module.exports = function(app) {
       
     }
     friends.push(req.body);  //push new form data
-    //console.log(bestMatch);
 
-    res.json(bestMatch); //post best match as JSON object
+    res.json(bestMatch);
   });
 
 }
